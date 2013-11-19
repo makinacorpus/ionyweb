3ce49eaddf2d050928f2020fc12863934efe173b
Gael Utard <gael.utard@makina-corpus.com>
2013-11-19 12:16:54 +0100

    Bug #626 : ionyweb plugin translation

--- ionyweb/plugin_app/plugin_contact/__init__.py
+++ ionyweb/plugin_app/plugin_contact/__init__.py
@@ -2,7 +2,7 @@
 from django.utils.translation import ugettext as _
 
 PLUGIN_INFO = {
-    'NAME': _(u"Contact"),
+    'NAME': u"Contact",
     'VERSION': "1.0",
     'SHORT_DESCRIPTION': _(u"Contact form with anwser by mail"),
     'DESCRIPTION': _(u"Sometime, it's better to do not show your email adresse of your website. You can propose a form to contact you. You will receive all details directly into your mailbox and will be able to anwser from there."),
